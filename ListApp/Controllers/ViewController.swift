6f3a061fa2aee8d12bf582173cd1375680c12f0e
Berkay Kuzu <92036779+Berkay-Kuzu@users.noreply.github.com>
2023-02-16 17:18:01 +0300

    UITableView

--- ListApp/Controllers/ViewController.swift
+++ ListApp/Controllers/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data = ["Swift", "C", "C++", "C#", "Java", "Kotlin"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
 
 }
 
