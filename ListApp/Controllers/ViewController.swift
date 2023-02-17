21e8445a3c2aa963b5992f6d06c1a34e3ca9edbc
Berkay Kuzu <92036779+Berkay-Kuzu@users.noreply.github.com>
2023-02-17 11:19:44 +0300

    Alert controller module system creates

--- ListApp/Controllers/ViewController.swift
+++ ListApp/Controllers/ViewController.swift
@@ -12,13 +12,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView!
     
     var data = [String]()
-
+    var alertController = UIAlertController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
@@ -30,69 +31,89 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     @IBAction func barButtonItemTapped(_ sender: UIBarButtonItem) {
+        presentAddAlert()
         
-        let alertController = UIAlertController(title: "You can add an item easily :)",
-                                                message: nil,
-                                                preferredStyle: .alert)
-        
-        let defaultButton = UIAlertAction(title: "Add",
-                                          style: .default) { _ in
-            let text = alertController.textFields?.first?.text
-            if text == "" {
-                let textAlert = UIAlertController(title: "Would you mind entering an item?",
-                                                  message: nil,
-                                                  preferredStyle: .alert)
-                let textAlertButton = UIAlertAction(title: "OK",
-                                                    style: .cancel)
-                textAlert.addAction(textAlertButton)
-                self.present(textAlert, animated: true)
-            } else {
-                self.data.append((text)!)
-                self.tableView.reloadData()
-            }
-        }
-        
-        let cancelButton = UIAlertAction(title: "Cancel",
-                                         style: .destructive)
-        
-        alertController.addTextField()
-        
-        alertController.addAction(defaultButton)
-        alertController.addAction(cancelButton)
-        present(alertController, animated: true)
     }
     
     @IBAction func didRemoveBarButtonItemTapped(_ sender: UIBarButtonItem) {
-        let alertController = UIAlertController(title: "Do you really want to delete all item in the list?",
-                                                message: nil,
-                                                preferredStyle: .alert)
-        let alertButton = UIAlertAction(title: "Delete",
-                                        style: .destructive) { _ in
+        presentAlert(titleInput: "Do you really want to delete all the item in the list?",
+                     messageIntput: nil,
+                     cancelAlertButtonInput: "Cancel",
+                     defaultButtonTitleInput: "Delete") { _ in
             self.data.removeAll()
             self.tableView.reloadData()
         }
-    
-        let alertButton2 = UIAlertAction(title: "No",
-                                         style: .default)
-        alertController.addAction(alertButton)
-        alertController.addAction(alertButton2)
-        
-        present(alertController, animated: true)
     }
     
     // to delete an item in the row!
-//    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-//        return .delete
-//    }
-//
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            tableView.beginUpdates()
-//            data.remove(at: indexPath.row)
-//            tableView.deleteRows(at: [indexPath], with: .fade)
-//            tableView.endUpdates()
-//        }
-//    }
+    //    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+    //        return .delete
+    //    }
+    //
+    //    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    //        if editingStyle == .delete {
+    //            tableView.beginUpdates()
+    //            data.remove(at: indexPath.row)
+    //            tableView.deleteRows(at: [indexPath], with: .fade)
+    //            tableView.endUpdates()
+    //        }
+    //    }
+    
+    func presentAddAlert() {
+        
+        presentAlert(titleInput: "You can add an item easily",
+                     messageIntput: nil,
+                     cancelAlertButtonInput: "No",
+                     defaultButtonTitleInput: "Add",
+                     isTextFieldAvailable: true) { _ in
+            let text = self.alertController.textFields?.first?.text
+            if text != "" {
+                self.data.append((text)!)
+                self.tableView.reloadData()
+                
+            } else {
+                self.presentWarningAlert()
+            }
+        }
+    }
+    
+    func presentWarningAlert() {
+        
+        presentAlert(titleInput: "Would you mind entering an item?",
+                     messageIntput: nil,
+                     cancelAlertButtonInput: "OK")
+    }
+    
+    func presentAlert(titleInput : String?,
+                      messageIntput: String?,
+                      prefferedStyleInput: UIAlertController.Style = .alert,
+                      cancelAlertButtonInput: String?,
+                      defaultButtonTitleInput: String? = nil,
+                      isTextFieldAvailable : Bool = false,
+                      defaultButtonHandlerInput: ((UIAlertAction) -> Void)? = nil ) {
+        
+        alertController = UIAlertController(title: titleInput,
+                                            message: messageIntput,
+                                            preferredStyle: prefferedStyleInput)
+        
+        if defaultButtonTitleInput != nil {
+            let defaultButton = UIAlertAction(title: defaultButtonTitleInput,
+                                              style: .default,
+                                              handler: defaultButtonHandlerInput)
+            alertController.addAction(defaultButton)
+        }
+        
+        
+        let cancelButton = UIAlertAction(title: cancelAlertButtonInput,
+                                         style: .destructive)
+        
+        if isTextFieldAvailable {
+            alertController.addTextField()
+        }
 
+        alertController.addAction(cancelButton)
+        
+        present(alertController, animated: true)
+    }
 }
 
